141955315ded1c58c14d03bc87218a5bbaf07c13
tatoclemente <tatoapuros@gmail.com>
2024-01-26 15:51:26 -0300

    Add comunication con Web Sockets

--- chat-app/src/ChatApp.jsx
+++ chat-app/src/ChatApp.jsx
@@ -1,11 +1,14 @@
 import { AuthProvider } from "./auth/AuthContext";
+import { SocketProvider } from "./context/SocketContext";
 import { AppRouter } from "./router/AppRouter";
 
 
 function ChatApp() {
   return (
     <AuthProvider>
-      <AppRouter />
+      <SocketProvider> // El contxto del socket necesita información de autenticación
+        <AppRouter />
+      </SocketProvider>
     </AuthProvider>
   );
 }
